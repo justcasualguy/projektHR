74641694699050de1cf8bc5e5e99b0cd91358338
justcasualguy <kubachw@gmail.com>
2019-11-13 18:31:44 +0100

    tests

---
+++ src/main/java/login/asasd.java
@@ -0,0 +1,15 @@
+package login;
+
+import javax.swing.*;
+import java.awt.event.*;
+public class asasd extends JDialog{
+    private JPanel panel1;
+    private JRadioButton radioButton1;
+    private JButton button;
+
+    public asasd() {
+        setContentPane(panel1);
+        setModal(true);
+        getRootPane().setDefaultButton(button);
+    }
+}
