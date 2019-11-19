cfd2c1485bc469edeeb0f7f55f1c601bc5c4f58f
justcasualguy <kubachw@gmail.com>
2019-11-19 23:49:36 +0100

    Application  can be built using fxml files(i guess)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,19 @@
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import generators.EmployeeGenerator;
-import org.bson.Document;
-
-
 public class Main {
     public static void main(String[] args) {
-        String databaseUri = "mongodb+srv://user:user@cluster0-xxoxs.azure.mongodb.net/test?retryWrites=true&w=majority&maxIdleTimeMS=5000&ssl=true";
-        String dbName = "Projekt";
-        MongoDatabase database = DBConnector.getDatabase(databaseUri,dbName);
-        MongoCollection<Document> collection = database.getCollection("Employees");
-
-
-        collection.insertOne(EmployeeGenerator.generateManEmployee().toDocument());
-
-
-        MongoCursor<Document> cur = collection.find().iterator();
-        while(cur.hasNext()){
-           // Document doc = cur.next();
-            System.out.println(cur.next());
-        }
+//        String databaseUri = "mongodb+srv://user:user@cluster0-xxoxs.azure.mongodb.net/test?retryWrites=true&w=majority&maxIdleTimeMS=5000&ssl=true";
+//        String dbName = "Projekt";
+//        MongoDatabase database = DBConnector.getDatabase(databaseUri,dbName);
+//        MongoCollection<Document> collection = database.getCollection("Employees");
+//
+//
+//        collection.insertOne(EmployeeGenerator.generateManEmployee().toDocument());
+//
+//
+//        MongoCursor<Document> cur = collection.find().iterator();
+//        while(cur.hasNext()){
+//           // Document doc = cur.next();
+//            System.out.println(cur.next());
+//        }
 
 
 
