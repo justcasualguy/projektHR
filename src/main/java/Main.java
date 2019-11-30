bad53966124a64bcb4ebeb98b80f6f28589b82d8
justcasualguy <kubachw@gmail.com>
2019-11-30 02:56:12 +0100

    Changed project structure Added utf-8 encoding to pom Changed DBConnector functioanlity Improved controllers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,53 @@
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import dbconnector.DBConnector;
-import org.bson.Document;
+import gui.MainStage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import services.dbconnector.DBConnector;
 
+public class Main extends Application {
+    public Stage mainStage = new Stage();
 
-public class Main {
     public static void main(String[] args) {
-        String databaseUri = "mongodb+srv://user:user@cluster0-xxoxs.azure.mongodb.net/test?retryWrites=true&w=majority&maxIdleTimeMS=5000&ssl=true";
-        String dbName = "Projekt";
-        MongoDatabase database = DBConnector.getDatabase(dbName);
-        MongoCollection<Document> collection = database.getCollection("Employees");
 
-
-       //collection.insertOne(EmployeeGenerator.generateManEmployee().toDocument());
-
-
-        MongoCursor<Document> cur = collection.find().iterator();
-        while(cur.hasNext()){
-           // Document doc = cur.next();
-            System.out.println(cur.next().toJson());
-        }
+//        String databaseUri = "mongodb+srv://user:user@cluster0-xxoxs.azure.mongodb.net/test?retryWrites=true&w=majority&maxIdleTimeMS=5000&ssl=true";
+//        String dbName = "Projekt";
+//        MongoDatabase database = DBConnector.getDatabase(databaseUri,dbName);
+//        MongoCollection<Document> collection = database.getCollection("Employees");
+//
+//
+//        collection.insertOne(EmployeeGenerator.generateManEmployee().toDocument());
+//
+//
+//        MongoCursor<Document> cur = collection.find().iterator();
+//        while(cur.hasNext()){
+//           // Document doc = cur.next();
+//            System.out.println(cur.next());
+//        }
 
 
 
 //        MongoCollection<Document> collection = mongoDatabase.getCollection("test");
 //        collection.insertOne(doc);
+        launch(args);
+
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        DBConnector.connect("Projekt");
+        Parent root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
+
+
+        Scene scene = new Scene(root);
+        mainStage= MainStage.mainStage;
+        mainStage.setScene(scene);
+        stage.setTitle("FXML Welcome");
+        //stage.setScene(scene);
+        mainStage.show();
+    }
+
+
 }
