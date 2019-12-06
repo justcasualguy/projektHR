8b8c41764e5d890fd6d4ea507e3b187f0ed16307
Gibczon <szampanek123@gmail.com>
2019-12-06 12:22:46 +0100

    Adding error generator, modifying userTableView.fxml to handle right click events, Added option to remove user, TableView is now refreshing after user is removed.

--- src/main/java/services/controllers/UserTableViewController.java
+++ src/main/java/services/controllers/UserTableViewController.java
@@ -1,20 +1,24 @@
 package services.controllers;
 
+import com.mongodb.WriteResult;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import models.User;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
 import services.dbconnector.DBConnector;
+import services.generators.ErrorGenerator;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class UserTableViewController implements Initializable {
@@ -134,4 +138,59 @@ public class UserTableViewController implements Initializable {
     {
         this.roleColumn = roleColumn;
     }
+
+    public void refreshTableView()
+    {
+        final ObservableList<User> dataList = FXCollections.observableArrayList();
+
+        dataList.addAll(DBConnector.getCollectionAsList(User.class));
+
+        FilteredList<User> filteredData = new FilteredList<>(dataList, b -> true);
+
+        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(user -> {
+                if(newValue == null || newValue.isEmpty()){
+                    return true;
+                }
+
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if(user.getUsername().toLowerCase().indexOf(lowerCaseFilter) != -1){
+                    return true;
+                } else return user.getRole().toLowerCase().indexOf(lowerCaseFilter) != -1;
+            });
+
+        });
+
+        SortedList<User> sortedData = new SortedList<>(filteredData);
+
+        sortedData.comparatorProperty().bind(findUserTableView.comparatorProperty());
+
+        findUserTableView.setItems(sortedData);
+    }
+
+    public void deleteUserAction(ActionEvent actionEvent)
+    {
+        User selectedUser = findUserTableView.getSelectionModel().getSelectedItem();
+        if(selectedUser == null)
+        {
+            ErrorGenerator.errorMessage("No user selected! Please select user.");
+            return;
+        }
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Removing user");
+        alert.setContentText("Are you sure you want to remove user: " + selectedUser.getUsername() + "?");
+        Optional<ButtonType> answer = alert.showAndWait();
+        Alert info = new Alert(Alert.AlertType.INFORMATION);
+        if(answer.get() == ButtonType.OK)
+        {
+            WriteResult wr = DBConnector.getDatastore().delete(selectedUser);
+            info.setContentText("User removed succesfully!");
+            info.show();
+            refreshTableView();
+        }
+
+    }
+
+
 }
---
+++ src/main/java/services/generators/ErrorGenerator.java
@@ -0,0 +1,37 @@
+package services.generators;
+
+import gui.MainStage;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ErrorGenerator
+{
+    public static void errorMessage(String error)
+    {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("ERROR OCCURED");
+        window.setMinWidth(300);
+
+        Label errorMessage = new Label();
+        errorMessage.setText(error);
+        Button close = new Button("Close");
+        close.setOnAction(e -> window.close());
+
+        VBox v = new VBox(20);
+        v.getChildren().addAll(errorMessage, close);
+        v.setAlignment(Pos.CENTER);
+
+
+        Scene scene = new Scene(v);
+        window.setScene(scene);
+        window.showAndWait();
+
+    }
+}
